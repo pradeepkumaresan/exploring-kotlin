3b2929e708294d6cae6aeb788acdc7eef8141357
Pradeep Kumaresan <pradeep.kumaresan@bridgei2i.com>
2020-06-07 08:47:08 +0530

    initial commit

--- src/NullableString.kt
+++ src/NullableString.kt
@@ -1,5 +1,7 @@
 fun main() {
     // val name:String = null  ERROR: non-nullable String
-    val name:String? = null // nullable String
+    var name:String? = null // nullable String
+    println(name)
+    name = "Pradeep"
     println(name)
 }
---
+++ src/TypeInferencesNullable.kt
@@ -0,0 +1,4 @@
+fun main() {
+    var name = null // Nothing type. Cannot infer to any type
+    // name = ""; // ERROR
+}
