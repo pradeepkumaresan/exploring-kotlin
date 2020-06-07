3cfc2b0fb325e25e2252df29f92e52d92c0e7141
Pradeep Kumaresan <pradeep.kumaresan@bridgei2i.com>
2020-06-07 08:37:43 +0530

    initial commit

---
+++ src/ImmutableVariablesVal.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val name:String = "Pradeep"
+    //name = "Kumaresan"  "val" cannot be reassigned. Use var instead.
+    println(name)
+}
---
+++ src/MutableVariablesVar.kt
@@ -0,0 +1,7 @@
+fun main() {
+    var name1:String = "Pradeep"
+    name1 = "Kumaresan"
+
+    println(name) // top level variable from TopLevelVariables.kt
+    println(name1)
+}
